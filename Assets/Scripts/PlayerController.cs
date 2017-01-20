31b2ac5d51059b901f50a9232a89e1a347439d68
sadflask <benjamin.n.frew@gmail.com>
2017-01-20 19:10:18 +1300

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Boundary
+{
+    public float xmin, xmax, ymin, ymax;
+}   
+
+public class PlayerController : MonoBehaviour {
+    public GameObject shot;
+    public float speed;
+    public Boundary boundary;
+    public float fireRate;
+    private float nextTime;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+
+    //Once per frame for physics
+    void FixedUpdate () {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal"),0,0) * speed;
+        rb.position = new Vector3
+           (
+               Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xmin, boundary.xmax),
+               Mathf.Clamp(GetComponent<Rigidbody>().position.y, boundary.ymin, boundary.ymax),
+               0
+           );
+    }
+
+    //Called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && (Time.time > nextTime))
+        {
+            Debug.Log("Fired" + Time.time);
+            nextTime = Time.time + fireRate;
+            Instantiate(shot, rb.position + new Vector3(0,5,0), Quaternion.identity);
+        }
+    }
+}
